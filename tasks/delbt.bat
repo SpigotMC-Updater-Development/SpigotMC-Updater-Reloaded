9247d303ae77d9c88afc558eafeabe5f10768935
ShadowCable <ShadowCable@thegearmc.com>
2016-04-28 17:58:49 -0600

    added gap to be better logging info

---
+++ tasks/delbt.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set startdir=%~dp0
+
+@echo Deleting old Buildtools.jar
+.echo
+if exist tasks\BuildTools.jar (del /f tasks\BuildTools.jar) else (@echo Buildtools.jar isnt located in tasks folder. Skipping this step and Downloading Buildtools.jar
+goto exit)
+.echo
+if exist tasks\BuildTools.jar (@echo Failed to delete old version. Make sure you have Read, Write, and Execute allowed in your directory) else (@echo Deleted old BuildTools.jar Sucessfully)
+.echo
+@echo Updating Buildtools.jar ;)
+.echo
+
+:exit
+exit

8e93e67dc0938685b2af5869ea2b3277d0dd25b4
ShadowCable <ShadowCable@thegearmc.com>
2016-04-28 17:52:41 -0600

    Uses commands tasks or program to be more informative

---
+++ tasks/help.bat
@@ -0,0 +1,38 @@
+@echo off
+
+echo.
+@echo ================ {Command List} ================
+echo.
+@echo task update: Updates Buildtools to latest version
+echo.
+@echo ------------------------------------------------
+echo.
+@echo help: Just added it if you want to spam it lol
+echo.
+@echo ------------------------------------------------
+echo.
+@echo task run: Runs Buildtools and saves APIs
+echo.
+@echo ------------------------------------------------
+echo.
+@echo task bungee: Updates BungeeCord and its modules
+echo.
+@echo ------------------------------------------------
+echo.
+@echo task clean: Deletes Buildtools files
+echo.
+@echo ------------------------------------------------
+echo.
+@echo task plugin: Runs Plugin-Fixer
+echo.
+@echo ------------------------------------------------
+echo.
+@echo program update: Updates this script
+echo.
+@echo ------------------------------------------------
+echo.
+@echo exit: Shuts Down Program
+echo.
+@echo ================ {Command List} ================
+echo.
+exit

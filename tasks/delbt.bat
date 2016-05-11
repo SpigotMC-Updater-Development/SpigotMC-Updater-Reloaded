90d4d3e2a21da33a9276c5418ed737625c46a4a4
ShadowCable <ShadowCable@thegearmc.com>
2016-05-11 16:43:45 -0600

    fixed error with .echo when it should be echo.

--- tasks/delbt.bat
+++ tasks/delbt.bat
@@ -7,14 +7,14 @@ If exist tasks/delbt.bat (goto pass) else (exit)
 set startdir=%~dp0
 
 @echo Deleting old Buildtools.jar
-.echo
+echo.
 if exist tasks\BuildTools.jar (del /f tasks\BuildTools.jar) else (@echo Buildtools.jar isnt located in tasks folder. Skipping this step and Downloading Buildtools.jar
 goto exit)
-.echo
+echo.
 if exist tasks\BuildTools.jar (@echo Failed to delete old version. Make sure you have Read, Write, and Execute allowed in your directory) else (@echo Deleted old BuildTools.jar Sucessfully)
-.echo
+echo.
 @echo Updating Buildtools.jar ;)
-.echo
+echo.
 
 :exit
 exit

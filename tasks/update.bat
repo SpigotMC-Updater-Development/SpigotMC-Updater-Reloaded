45e097b62aa842cd2d1519c80f59acbb66558f49
Legoman99573 <christianwhead@gmail.com>
2016-05-23 14:09:47 -0600

    Update and rename update.bat to tasks/update.bat

--- tasks/update.bat
+++ tasks/update.bat
@@ -5,26 +5,30 @@ goto run) else (exit)
 
 :run
 
+set startdir=%~dp0
+
+cd %startdir
+
 set v=
-for /f "delims=" %%i in ('type tasks\version.txt') do set v=%%i
+for /f "delims=" %%i in ('type version.txt') do set v=%%i
 
 set content=
-for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
+for /f "delims=" %%i in ('type ..\config\gitlocation.txt') do set content=%%i
 
 @echo Checking for Updates...
 
-powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/btversion.txt -OutFile tasks/version_dummy.txt
+powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/btversion.txt -OutFile version_dummy.txt
 
 @echo Current Version
-findstr /c:"%v%" /i tasks\version.txt
+findstr /c:"%v%" /i version.txt
 set RESULT1=%ERRORLEVEL%
 echo.
 echo.
 @echo Current Update Version.
-findstr /c:"%v%" /i tasks\version_dummy.txt
+findstr /c:"%v%" /i version_dummy.txt
 set RESULT2=%ERRORLEVEL%
 set v2=
-for /f "delims=" %%i in ('type tasks\version_dummy.txt') do set v2=%%i
+for /f "delims=" %%i in ('type version_dummy.txt') do set v2=%%i
 echo.
 cls
 if %RESULT1%==%RESULT2% (
@@ -40,11 +44,11 @@ if %RESULT1%==%RESULT2% (
     del /f convert.bat
     
     @echo Making version_dummy.txt merge to version.txt
-    del /f tasks\version.txt
-    del /f tasks\version_dummy.txt
+    del /f version.txt
+    del /f version_dummy.txt
     cls
     @echo Finished Updating to v.%v2%
-    powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/btversion.txt -OutFile tasks/version.txt
+    powershell -command Invoke-WebRequest -Uri http://thegearmc.com/update/btversion.txt -OutFile version.txt
 )
 :exit
 echo.

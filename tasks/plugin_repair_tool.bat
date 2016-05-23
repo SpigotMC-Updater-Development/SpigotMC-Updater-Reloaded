c35dbe9571e0495b2732bbc4027da3d1bd38d0d7
Legoman99573 <christianwhead@gmail.com>
2016-05-23 14:04:11 -0600

    moved to tasks

--- tasks/plugin_repair_tool.bat
+++ tasks/plugin_repair_tool.bat
@@ -4,27 +4,30 @@ if exist tasks/session.txt (del /f tasks\session.txt
 goto boot) else (exit)
 
 :boot
+%content% --login -i -c "cd /tasks/"
+
+set startdir=%~dp0
 
 set content=
-for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
+for /f "delims=" %%i in ('type ..\config\gitlocation.txt') do set content=%%i
 
 @echo Welcome to Plugin Fixer.
 %content% --login -i -c "sleep 1s"
 :again
 cls
 @echo Currently Editing plugin.txt
-start "SpigotMC Updater | Plugin Configuration" /wait config/plugin.txt
+start "SpigotMC Updater | Plugin Configuration" /wait ../config/plugin.txt
 %content% --login -i -c "sleep 5s"
 cls
 set plugin=
-for /f "delims=" %%i in ('type config\plugin.txt') do set plugin=%%i
+for /f "delims=" %%i in ('type ..\config\plugin.txt') do set plugin=%%i
 
 echo.
 if exist %plugin%.jar (@echo Found %plugin%.jar. Using Special Mapping Methods.
-%content% --login -i -c "java -jar tasks/Buildtools_Files/BuildData/bin/SpecialSource-2.jar map -m tasks/Buildtools_Files/CraftBukkit/deprecation-mappings.csrg -i plugin/%plugin%.jar -o plugin/%plugin%-fixed.jar"
+%content% --login -i -c "java -jar Buildtools_Files/BuildData/bin/SpecialSource-2.jar map -m Buildtools_Files/CraftBukkit/deprecation-mappings.csrg -i ../plugin/%plugin%.jar -o ../plugin/%plugin%-fixed.jar"
 @echo Moving %plugin%.jar to plugin\plugin-dump
 %content% --login -i -c "sleep 5s"
-move %plugin%.jar plugin\plugin-dump) else (@echo Could not find %plugin%.jar in folder %startdir%plugin)
+move ..\plugin\%plugin%.jar ..\plugin\plugin-dump) else (@echo Could not find %plugin%.jar in folder plugin)
 echo.
 @echo Do you want to run this again: (Y, N)
 Set /P "_1=" || Set _1=NothingChosen

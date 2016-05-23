7eacebe00567ea6d7abaf650da2bcde97fd08466
ShadowCable <ShadowCable@thegearmc.com>
2016-05-23 15:42:22 -0600

    now gives helpful tools for reporting pugs, etc

--- tasks/Buildtools_Files/run.bat
+++ tasks/Buildtools_Files/run.bat
@@ -2,6 +2,8 @@
 
 if exist tasks\Buildtools_Files\run.bat (goto run) else (exit)
 
+:run
+
 set startdir=%~dp0
 
 echo %startdir%
@@ -17,5 +19,22 @@ for /f "delims=" %%i in ('type config\gitlocation.txt') do set content=%%i
 cd %startdir%
 
 %content% --login -i -c "java -jar BuildTools.jar --rev %version%"
+
+set datetime=
+for /f %%a in ('powershell -Command "Get-Date -format yyyy_MM_dd__HH_mm_ss"') do set datetime=%%a
+
+if exist BuildTools.log.txt (@echo Renaming BuildTools.log.txt to BuildTools.%datetime%.txt
+rename BuildTools.log.txt Buildtools.%datetime%.txt
+move BuildTools.*.txt ..\temp
+echo.
+cls
+@echo These logs were renamed to help with reporting bugs on https://hub.spigotmc.org/jira/projects/BUILDTOOLS/issues
+echo.
+@echo Report server bugs on https://hub.spigotmc.org/jira/projects/SPIGOT/issues
+echo.
+@echo Need further assistance, just go to the IRC Channel to get support or just use our buildtools file cleaner
+)
+
+%content% --login -i -c "sleep 10s"
 
 exit

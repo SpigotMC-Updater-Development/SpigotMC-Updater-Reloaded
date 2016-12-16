0bb160fdcffd6721665dede6145606437474c9ce
Legoman99573 <christianwhead@gmail.com>
2016-12-16 12:35:51 -0600

    Bug Fixes and Fixes against Windows 10 Anniversary Update.

--- tasks/delbt.bat
+++ tasks/delbt.bat
@@ -1,20 +1,49 @@
 @echo off
 
-If exist tasks/delbt.bat (goto pass) else (exit)
+If exist tasks/delbt.bat (
+    goto pass
+) else (
+    exit
+)
 
 :pass
 
 set startdir=%~dp0
 
+cd %startdir%
+
+set content=..\Git\bin\bash.exe
+
 @echo Deleting old Buildtools.jar
-echo.
-if exist tasks\BuildTools.jar (del /f tasks\BuildTools.jar) else (@echo Buildtools.jar isnt located in tasks folder. Skipping this step and Downloading Buildtools.jar
-goto exit)
-echo.
-if exist tasks\BuildTools.jar (@echo Failed to delete old version. Make sure you have Read, Write, and Execute allowed in your directory) else (@echo Deleted old BuildTools.jar Sucessfully)
-echo.
-@echo Updating Buildtools.jar ;)
-echo.
+@echo [Info] Deleting old Buildtools.jar >> ..\log.txt
+
+%content% --login -i -c "sleep 5s"
+if exist BuildTools_Files/BuildTools.jar (
+    del /f BuildTools_Files\BuildTools.jar
+) else (
+    powershell.exe -command write-host "Buildtools.jar isnt located in %startdir%Buildtools_Files\. Skipping this step and Downloading Buildtools.jar." -f yellow
+    @echo [Warning] Buildtools.jar isnt located in %startdir%Buildtools_Files\. Skipping this step and Downloading Buildtools.jar >> ..\log.txt
+    %content% --login -i -c "sleep 5s"
+    goto exit
+)
+
+%content% --login -i -c "sleep 5s"
+
+cls
+if exist BuildTools_Files/BuildTools.jar (
+    powershell.exe -command write-host "Failed to delete old version. Make sure you have Read, Write, and Execute allowed in %startdir%Buildtools_Files\." -f red
+    @echo [ERROR] Failed to delete old version. Make sure you have Read, Write, and Execute allowed in %startdir%Buildtools_Files\. >> ..\log.txt
+) else (
+    powershell.exe -command write-host "Deleted BuildTools.jar from %startdir%Buildtools_Files\ Sucessfully." -f green
+    @echo [Info] Deleted BuildTools.jar from %startdir%Buildtools_Files\ Sucessfully. >> ..\log.txt
+)
+
+%content% --login -i -c "sleep 5s"
+cls
+
+@echo Updating Buildtools.jar :D
+@echo [Info] Updating Buildtools.jar :D >> ..\log.txt
 
 :exit
+cd ..\
 exit

dc8b34bc7b526c608e66ebbd8d468252d4bd1dee
Legoman99573 <christianwhead@gmail.com>
2016-12-20 11:22:55 -0600

    Added checks and left a disclaimer message ;)

--- tasks/plugin_repair_tool.bat
+++ tasks/plugin_repair_tool.bat
@@ -1,41 +1,74 @@
 @echo off
 
-if exist tasks/session.txt (del /f tasks\session.txt
-goto boot) else (exit)
+if exist tasks/session.txt (
+	del /f tasks\session.txt
+	goto boot
+) else (
+	exit
+)
 
 :boot
-%content% --login -i -c "cd /tasks/"
 
 set startdir=%~dp0
 
-set content=
-for /f "delims=" %%i in ('type ..\config\gitlocation.txt') do set content=%%i
+cd %startdir%
 
-@echo Welcome to Plugin Fixer.
-%content% --login -i -c "sleep 1s"
-:again
-cls
-@echo Currently Editing plugin.txt
-start "SpigotMC Updater | Plugin Configuration" /wait ../config/plugin.txt
-%content% --login -i -c "sleep 5s"
-cls
+set content= ..\Git\bin\bash.exe
+
 set plugin=
 for /f "delims=" %%i in ('type ..\config\plugin.txt') do set plugin=%%i
 
-echo.
-if exist %plugin%.jar (@echo Found %plugin%.jar. Using Special Mapping Methods.
-%content% --login -i -c "java -jar Buildtools_Files/BuildData/bin/SpecialSource-2.jar map -m Buildtools_Files/CraftBukkit/deprecation-mappings.csrg -i ../plugin/%plugin%.jar -o ../plugin/%plugin%-fixed.jar"
-@echo Moving %plugin%.jar to plugin\plugin-dump
-%content% --login -i -c "sleep 5s"
-move ..\plugin\%plugin%.jar ..\plugin\plugin-dump) else (@echo Could not find %plugin%.jar in folder plugin)
-echo.
-@echo Do you want to run this again: (Y, N)
-Set /P "_1=" || Set _1=NothingChosen
-If "%_1%"=="NothingChosen" goto :stop
-If /i "%_1%"=="Y" goto again
-If /i "%_1%"=="y" goto again
-If /i "%_1%"=="N" goto stop
-If /i "%_1%"=="n" goto stop
+powershell.exe -command write-host "This tool may not fix all jar issues. We recommend updating your code manually or find alternates of the plugin that does not work." -f yellow
+@echo [WARNING] This tool may not fix all jar issues. We recommend updating your code manually or find alternates of the plugin that does not work. >> ..\log.txt
+%content% --login -i -c "sleep 20s"
 
-:stop
+cls
+
+if exist Buildtools_Files/BuildData/bin/SpecialSource-2.jar (
+	powershell.exe -command write-host "Found SpecialSource-2.jar." -f green
+	@echo [Info] Found SpecialSource-2.jar. >> ..\log.txt
+) else (
+	powershell.exe -command write-host "SpecialSource-2.jar was not found. Maybe you have not ran BuildTools.jar before or Spigot no longer supports it." -f red
+	@echo [ERROR] SpecialSource-2.jar was not found. Maybe you have not ran BuildTools.jar before or Spigot no longer supports it. >> ..\log.txt
+)
+
+%content% --login -i -c "sleep 5s"
+if exist Buildtools_Files/CraftBukkit/deprecation-mappings.csrg (
+	powershell.exe -command write-host "Found deprecation-mappings.csrg." -f green
+	@echo [Info] Found deprecation-mappings.csrg. >> ..\log.txt
+) else (
+	powershell.exe -command write-host "deprecation-mappings.csrg was not found. Maybe you have not ran BuildTools.jar before or Spigot no longer supports it." -f red
+	@echo [ERROR] deprecation-mappings.csrg was not found. Maybe you have not ran BuildTools.jar before or Spigot no longer supports it. >> ..\log.txt
+)
+
+if exist %plugin%.jar (
+	powershell.exe -command write-host "Found %plugin%.jar. Using Special Mapping Methods." -f green
+	@echo [Info] Found %plugin%.jar. Using Special Mapping Methods. >> ..\log.txt
+	%content% --login -i -c "java -jar Buildtools_Files/BuildData/bin/SpecialSource-2.jar map -m Buildtools_Files/CraftBukkit/deprecation-mappings.csrg -i ../plugin/%plugin%.jar -o ../plugin/%plugin%-fixed.jar"
+	
+	%content% --login -i -c "sleep 5s"
+	if exist ../plugin/%plugin%-fixed.jar (
+		@echo Moving %plugin%.jar to plugin\plugin-dump
+		%content% --login -i -c "sleep 5s"
+		move ..\plugin\%plugin%.jar ..\plugin\plugin-dump
+		
+		%content% --login -i -c "sleep 5s"
+		if exist ../plugin/plugin-dump/%plugin%.jar (
+			powershell.exe -command write-host "%plugin%.jar was moved succesfully." -f green
+			@echo [Info] %plugin%.jar was moved succesfully. >> ..\log.txt
+		) else (
+			powershell.exe -command write-host "Failed to move %plugin%.jar. Make sure you have read and write access to the plugin and plugin-dump folders." -f red
+			@echo [ERROR] Failed to move %plugin%.jar. Make sure you have read and write access to the plugin and plugin-dump folders. >> ..\log.txt
+		)
+		
+	) else (
+		powershell.exe -command write-host "The program has failed to fix the jar using SpecialSource jar. Would recommend checking your code and edit it manually." -f yellow
+		@echo [WARNING] The program has failed to fix the jar using SpecialSource jar. Would recommend checking your code and edit it manually. >> ..\log.txt
+		exit
+	)
+	
+) else (
+	@echo [ERROR] Could not find %plugin%.jar in folder plugin. >> ..\log.txt
+)
+
 exit

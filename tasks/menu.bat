f6064dc3d42e32ce9b8c3f39ef6a7cda14761668
Legoman99573 <christianwhead@gmail.com>
2016-12-26 21:06:26 -0600

    Added Nukkit Support

--- tasks/menu.bat
+++ tasks/menu.bat
@@ -30,18 +30,18 @@ title SpigotMC Updater v.%v%
 :load
 cls
 
-powershell.exe -command write-host "Welcome to SpigotMC Updater v.%v% `r`n `r`noOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOo `r`n What task do you want to run? Must use numbers and press enter. `r`n 1. Update Buildtools `r`n 2. Run Buildtools `r`n 3. Cleans BuildTools Folder `r`n 4. Repair a plugin with the Spigot Special Recipe `r`n 5. Grab BungeeCord `r`n 6. Grabs PaperSpigot `r`n 7. Report a bug in this script `r`n 8. Close this script safely `r`noOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOo" -f darkyellow
+powershell.exe -command write-host "Welcome to SpigotMC Updater v.%v% `r`n `r`noOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOo `r`n What task do you want to run? Must use numbers and press enter. `r`n 1. Update Buildtools `r`n 2. Run Buildtools `r`n 3. Cleans BuildTools Folder `r`n 4. Repair a plugin with the Spigot Special Recipe `r`n 5. Grab BungeeCord `r`n 6. Grabs PaperSpigot `r`n 7. Grabs Nukkit `r`n 8. Report a bug in this script `r`n 9. Close this script safely `r`noOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOoOo" -f darkyellow
 
-Set /P "_1=>" || Set _1=NothingChosen
-If "%_1%"=="NothingChosen" goto :commandnotfound
-If /i "%_1%"=="1" goto update
-If /i "%_1%"=="2" goto run
-If /i "%_1%"=="3" goto clean
-If /i "%_1%"=="4" goto plugin
-If /i "%_1%"=="5" goto bungee
-If /i "%_1%"=="6" goto paper
-If /i "%_1%"=="7" goto bug
-If /i "%_1%"=="8" goto exit
+CHOICE /C 123456789 /N /M "> "
+IF ERRORLEVEL 9 GOTO exit
+IF ERRORLEVEL 8 GOTO bug
+IF ERRORLEVEL 7 GOTO nukkit
+IF ERRORLEVEL 6 GOTO paper
+IF ERRORLEVEL 5 GOTO bungee
+IF ERRORLEVEL 4 GOTO plugin
+IF ERRORLEVEL 3 GOTO clean
+IF ERRORLEVEL 2 GOTO run
+IF ERRORLEVEL 1 GOTO update
 
 :commandnotfound
 @echo Command not found. Try help for help menu
@@ -71,6 +71,11 @@ goto start
 :paper
 cls
 start /b /wait tasks\paper.bat
+goto start
+
+:nukkit
+cls
+start /b /wait tasks\nukkit.bat
 goto start
 
 :bug
